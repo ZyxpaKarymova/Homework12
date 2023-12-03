6d315aab32e2a715254e750b7436f153224ee9f5
ZyxpaKarymova <z_karimova@list.ru>
2023-12-03 17:05:12 +0300

    Commit

---
+++ src/main/java/sky/pro/Author.java
@@ -0,0 +1,20 @@
+package sky.pro;
+
+public class Author {
+    String name;
+    String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+}
+
